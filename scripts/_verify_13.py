44f95fdd9049e1c003e3f3470588bf0ffa546751
fly <yao.liu@tvbc.com.cn>
2026-08-27 19:08:41 +0800

    chore: 删除一次性数据校验临时脚本 _verify_*.py

--- scripts/_verify_13.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import pdfplumber, warnings
-warnings.filterwarnings('ignore')
-with pdfplumber.open(r'data\pdfs\09992\09992_2025_中报.pdf') as pdf:
-    for pg in [78, 79]:
-        t = pdf.pages[pg-1].extract_text() or ''
-        print(f"----- p{pg} -----")
-        print(t[:3800])
-        print()
